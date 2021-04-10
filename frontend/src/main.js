edd29adad31a9fe3b34cda5667aa64be3e8e419b
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-04-10 15:39:46 -0700

    formatting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,10 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 // Sentry.init({ dsn: 'https://50ca82c37131420b9e5deead3ba0695a@sentry.io/4977551' });
 
 const app = new App({
-	target: document.body,
-	props: {}
+  target: document.body,
+  props: {}
 });
 
 export default app;
