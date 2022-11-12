02e26b7511347f8feb6816c6d7db2d01aa37cbc0
Bradley J. Spaulding <brad.spaulding@gmail.com>
2022-11-12 13:48:51 -0800

    Updated svelte project to latest

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,8 @@
-import App from "./App.svelte";
-
-// Sentry.init({ dsn: 'https://50ca82c37131420b9e5deead3ba0695a@sentry.io/4977551' });
+import './app.css'
+import App from './App.svelte'
 
 const app = new App({
-  target: document.body,
-  props: {}
-});
+  target: document.getElementById('app')
+})
 
-export default app;
+export default app
